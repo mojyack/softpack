95c18b654bd7caa735266bc1585505757cb22f07
mojyack <mojyack@gmail.com>
2023-12-05 02:47:33 +0900

    cmake release build

--- repo/_common/cmake.sh
+++ repo/_common/cmake.sh
@@ -1,5 +1,5 @@
 cmake_configure() {
     mkdir -p $1
     cd $1
-    cmake -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_PREFIX=$prefix ..
+    cmake -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_BUILD_TYPE=Release ..
 }
