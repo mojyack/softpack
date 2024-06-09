8e9e6bd14db0ea398e4933d13d6386755870c3b4
mojyack <mojyack@gmail.com>
2024-06-09 16:30:48 +0900

    add libtatsu

---
+++ repo/libtatsu/libtatsu-git.sh
@@ -0,0 +1,20 @@
+src=(
+    "https://github.com/libimobiledevice/$pkg.git"
+)
+
+project_root=$pkg.git
+
+source "$repo/_common/regular-make.sh"
+
+configure() {
+    NOCONFIGURE=1 ./autogen.sh
+    regular_configure
+}
+
+build() {
+    regular_make_all
+}
+
+install() {
+    regular_make_install
+}
